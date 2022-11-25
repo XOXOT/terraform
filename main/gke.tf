8d3b20d010825070a7c8d2af6ff02fb3700eb0cb
XOXOT <1418083@donga.ac.kr>
2022-11-25 07:45:38 +0000

    edit gke

--- main/gke.tf
+++ main/gke.tf
@@ -10,17 +10,18 @@ resource "google_container_cluster" "primary" {
   network    = data.terraform_remote_state.bucket.outputs.xoxot_vpc.name
   subnetwork = data.terraform_remote_state.bucket.outputs.xoxot_subnet.name
 
-  cluster_autoscaling { 
-    enabled = true
-    resource_limits {
-      resource_type = "cpu" 
-      maximum = "500"
-    }
-    resource_limits {
-      resource_type = "memory"
-      maximum = "500"
-    }
-  }
+# ##이건 노드풀을 오토스케일링 하는 것임
+#   cluster_autoscaling { 
+#     enabled = true
+#     resource_limits {
+#       resource_type = "cpu" 
+#       maximum = "500"
+#     }
+#     resource_limits {
+#       resource_type = "memory"
+#       maximum = "500"
+#     }
+#   }
 
   private_cluster_config {
     enable_private_nodes    = true
